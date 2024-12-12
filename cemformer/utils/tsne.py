b0b6ee8d6d645e10aafe789be8cac00f1dc1c97e
mukilan <mukilan.nitt@gmail.com>
2024-12-12 10:18:32 +0530

    added modification

--- cemformer/utils/tsne.py
+++ cemformer/utils/tsne.py
@@ -3,7 +3,7 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
-
+print("hi")
 class plot_tsne():
 
     def __init__(self,perplexity=10,random_state=42,n_comp=2):
